39341edc43e99c53d0c3f276d4f91fce18450423
CodingAndTechnology <CodingAndTechnology@aol.com>
2024-03-27 19:27:40 +0530

    fixed formating issues area program

--- area.py
+++ area.py
@@ -1,11 +1,12 @@
 class Rectangle:
-    def __init__(self, w, l):
+    def __init__(self, w, b):
         self.width = w
-        self.length = l
+        self.length = b
 
     def area(self):
         return self.width * self.length
 
 r = Rectangle(2.2, 1.4)
+
 a = r.area()
 print(f"Area is {a}")
